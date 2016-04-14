a8faa3ebb512b5ba60f625fb79d1bc5205be7afd
Carlos Velasquez <cv.velasquez@gmail.com>
2016-04-13 19:33:37 -0700

    normailze

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :status, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :status, :password, :password_confirmation, :avatar)
   end
 
 end
